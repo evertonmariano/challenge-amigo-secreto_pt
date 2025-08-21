fa46d93bdd6eeb4e55f181c1c3821fa70e91f07f
Everton Mariano <emnunes_pb@hotmail.com>
2025-08-21 17:03:39 -0300

    Implementei uma função para agregar amigos

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 // Array que armazena os amigos
-    let amigos = [];
+  let amigos = [];
 
-    
+  function adicionarAmigo() {
+    // Captura o valor do campo de entrada
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    // Validação: campo vazio
+    if (nome === "") {
+      alert("Por favor, insira um nome.");
+      return;
+      }
+
+    // Atualiza o array
+    amigos.push(nome);
+
+    // Atualiza a exibição da lista na tela
+    atualizarLista();
+
+    // Limpa o campo de entrada
+    input.value = "";
+  }
+
